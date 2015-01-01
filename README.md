fb99db0d2efb42107b53207927461b74dcb11d44
Steven Vachon <contact@svachon.com>
2015-01-01 13:30:16 -0500

    v0.0.3

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
-# handlebars-react
-> Compile Handlebars templates to [React](https://facebook.github.io/react/).
+# handlebars-html-parser
+> Parse Handlebars and HTML.
+
+Parse this:
+```handlebars
+<{{tag}}>value</{{tag}}>
+```
+into this:
+```js
+[
+    { type:"htmlTagStart", closing:false },
+    { type:"htmlTagNameStart" },
+    { type:"hbsVariable", id:"tag" },
+    { type:"htmlTagNameEnd" },
+    { type:"text", text:"value" },
+    { type:"htmlTagStart", closing:true },
+    { type:"htmlTagNameStart" },
+    { type:"hbsVariable", id:"tag" },
+    { type:"htmlTagNameEnd" },
+    { type:"htmlTagEnd" }
+]
+```
+…for use in compiling to something like a virtual DOM, such that of [handlebars-react](https://github.com/stevenvachon/handlebars-react).
+
+For more information on the strengths, weaknesses and implementation details of this library, check out the [wiki](https://github.com/stevenvachon/handlebars-html-parser/wiki).
 
 ## FAQ
-1. **Why not use [parse5](https://npmjs.com/package/parse5)?**  
+1. **How is this different from [HTMLBars](https://github.com/tildeio/htmlbars)?**  
+HTMLBars *builds* a DOM whereas this library *allows* you to build a DOM and even a virtual DOM.
+
+2. **Why not use [parse5](https://npmjs.com/package/parse5)?**  
 As of v1.2.0, it is not forgiving enough, in that it will parse `<{{tag}}>asdf</{{tag}}>` as text instead of HTML.
 
 ## Roadmap Features
@@ -11,12 +37,5 @@ As of v1.2.0, it is not forgiving enough, in that it will parse `<{{tag}}>asdf</
 * `options.ignoreComments` to omit html comments from compiling
 * `options.mustacheOnly` that disables helpers
 
-## api idea
-https://github.com/isaacs/sax-js#usage
-
-## ast type ideas
-https://github.com/blakeembrey/dombars/blob/master/lib/compiler/base.js
-https://github.com/mikepb/jade-react-compiler/blob/master/lib/builder.js
-
-## template idea
-https://github.com/duncanbeevers/jade-react
+## Changelog
+* 0.0.1–0.0.3 pre-releases
--- test/test.js
+++ test/test.js
@@ -1,86 +1,9 @@
 var fs = require("fs");
-var log = require("./devlog.js").log;
+var devlog = require("../lib/devlog");
 var parser = require("../lib");
 
 var test = __dirname+"/templates/test.hbs";
 //var test = __dirname+"/templates/test.html";
 test = fs.readFileSync(test, {encoding:"utf8"});
 
-console.log(test);
-
-//return require("dombars").compile(test)({});
-
-test = new parser(
-{
-	hbsComment: function(text)
-	{
-		// text == 'comment text';
-	},
-	hbsHelperStart: function(id, params)
-	{
-		log(["hbsHelperStart", id, params]);
-		// type == 'if';
-		// params == ['variable1'];
-	},
-	hbsHelperEnd: function(id)
-	{
-		log(["hbsHelperEnd", id]);
-		// text == 'if';
-	},
-	hbsSectionStart: function(id)
-	{
-		log(["hbsSectionStart", id]);
-	},
-	hbsSectionEnd: function(id)
-	{
-		log(["hbsSectionEnd", id]);
-	},
-	hbsVariable: function(id)
-	{
-		log(["hbsVariable", id]);
-	},
-	htmlAttributeStart: function(attributeName, attributeValue)
-	{
-		log(["htmlAttributeStart", attributeName, attributeValue]);
-		// attributeName == 'data-attr';
-		// encapsulator == '"' || "'" || '';
-	},
-	/*htmlAttributeEnd: function(encapsulator)
-	{
-		// encapsulator == '"' || "'" || '';
-	},
-	htmlAttributeValue: function(text)
-	{
-		// text == 'value3 value4';
-	},*/
-	htmlComment: function(text)
-	{
-		// text == 'comment text';
-	},
-	htmlTagStart: function(tagName/*, hasAttributes*/)
-	{
-		log(["htmlTagStart", tagName]);
-		// React.DOM[tagName] || React.createElement(tagName)
-		// tagName == 'span';
-		// hasAttributes == true;
-	},
-	htmlTagEnd: function(tagName/*, selfClosing*/)
-	{
-		log(["htmlTagEnd", tagName]);
-		// tagName == 'span';
-		// selfClosing == false;
-	},
-	text: function(text)
-	{
-		log(["text", text]);
-		// text == 'value5';
-	},
-	processingInstruction: function(name, data)
-	{
-		log(["processingInstruction", name, data]);
-	},
-	error: function(error)
-	{
-		log(["error", error]);
-	}
-}).parse(test);
+new parser().parse(test);
